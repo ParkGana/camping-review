f665c59ce89bc9e67d98b890bb8ba6a74f3034af
ParkGana <gnchoco97@gmail.com>
2023-12-03 03:17:30 +0900

    color, typography 정의

---
+++ src/configuration/color.ts
@@ -0,0 +1,13 @@
+export const Color = {
+    white: '#FFFFFF',
+
+    black: '#000000',
+
+    lightgray: '#C5CAD2',
+
+    gray: '#91979D',
+
+    red: '#FF5F63',
+
+    blue: '#0584F3'
+}
---
+++ src/configuration/typography.ts
@@ -0,0 +1,55 @@
+export const Typography = {
+    headline1: {
+        fontSize: 36,
+        fontWeight: '900' as '900',
+        lineHeight: 40
+    },
+
+    headline2: {
+        fontSize: 30,
+        fontWeight: '900' as '900',
+        lineHeight: 34
+    },
+
+    headline3: {
+        fontSize: 24,
+        fontWeight: '900' as '900',
+        lineHeight: 28
+    },
+
+    title1: {
+        fontSize: 24,
+        fontWeight: '700' as '700',
+        lineHeight: 28
+    },
+
+    title2: {
+        fontSize: 20,
+        fontWeight: '700' as '700',
+        lineHeight: 24
+    },
+
+    title3: {
+        fontSize: 16,
+        fontWeight: '700' as '700',
+        lineHeight: 20
+    },
+
+    body1: {
+        fontSize: 16,
+        fontWeight: '500' as '500',
+        lineHeight: 20
+    },
+
+    body2: {
+        fontSize: 14,
+        fontWeight: '500' as '500',
+        lineHeight: 18
+    },
+
+    body3: {
+        fontSize: 12,
+        fontWeight: '500' as '500',
+        lineHeight: 16
+    }
+}
